c777d45f54c4f38c499dad285ea95bc80d60c5c6
Chayanis Settapong <Chayanis.set@gmail.com>
2023-06-06 03:00:00 +0700

    Remove files

--- android/app/src/main/kotlin/com/google/firebase/presents/wireless/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.google.firebase.presents.wireless
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/shared/api_consts.dart
+++
@@ -1,3 +0,0 @@
-String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
-// GPT APT KEY
